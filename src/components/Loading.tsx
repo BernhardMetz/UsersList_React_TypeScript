4ff83509ee66a52913b31ae6c6e4d62dc62b30ae
talentedbernhard@gmail.com <talentedbernhard@gmail.com>
2020-02-13 15:58:44 +0100

    loading component was created

---
+++ src/components/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+export const Loading: React.FC<Props> = () => {
+        return (
+            <div>
+                
+            </div>
+        )
+}
