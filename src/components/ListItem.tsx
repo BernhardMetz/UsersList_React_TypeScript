a74fb7d6410bf9c8b6bedf9db6a2ae5c309d0a32
talentedbernhard@gmail.com <talentedbernhard@gmail.com>
2020-02-16 22:13:27 +0100

    demo was completed

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -6,11 +6,13 @@ import { ItemProps } from "../interfaces/types";
 export const ListItem: React.FC<ItemProps> = props => {
 
   return (
-    <div ref={props.refElement} className="list-item">
-      <img src={props.avatarURL} alt="avatar" />
-      <span>
-        {props.firstName} {props.lastName}
-      </span>
+    <div className="list-item-box">
+      <div ref={props.refElement} className="list-item">
+        <img src={props.avatarURL} alt="avatar" />
+        <span>
+          {props.firstName} {props.lastName}
+        </span>
+      </div>
     </div>
   );
 };
